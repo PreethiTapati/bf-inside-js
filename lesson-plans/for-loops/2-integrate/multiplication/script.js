0477591501987fd01e8d3106cf215ecd33d32196
Preethi Tapati <118854537+PreethiTapati@users.noreply.github.com>
2023-06-13 12:57:02 +0200

    Update script.js

--- lesson-plans/for-loops/2-integrate/multiplication/script.js
+++ lesson-plans/for-loops/2-integrate/multiplication/script.js
@@ -7,12 +7,8 @@ document.getElementById('do-math').addEventListener('click', () => {
   const num2 = readNumber('right');
 
   // use a for loop to multiply the two numbers
-  //const multiply = num1 * num2
-  let multiply = 0;
-  for (let i = 0; i < num2; i++) {
-    multiply += num1;
-  }
-
+  const multiply = num1 * num2
+  
 
   // display the product
   display('product', multiply);
